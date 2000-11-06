3de1e7d6403859d1e3e4b31dd4b0d683ba296252
Eric <273086429@qq.com>
2000-11-06 20:00:00 +0800

    Add something

---
+++ 30/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func f1() (r int) {
+	defer func() {
+		r++
+	}()
+	return 0
+}
+
+func f2() (r int) {
+	t := 5
+	defer func() {
+		t = t + 5
+	}()
+	return t
+}
+
+func f3() (r int) {
+	defer func(r int) {
+		r = r + 5
+	}(r)
+	return 1
+}
+
+func main() {
+	fmt.Println(f1(), f2(), f3())
+}
